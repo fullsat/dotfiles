f0af20b629abcc5c72181a31b23d828d5fbeb6d1
fullsat <fullsat310@gmail.com>
2018-04-21 13:43:18 +0900

    install neo bundle at install.

--- bin/set.sh
+++ bin/set.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 
-#brew install zsh-completions
 BACKUPDATE=$(date +%Y%m%d%H%M%S)
 BASEDIR="$(cd $(dirname $0) && pwd)/.."
 
+
+# zshの追加補間
+echo "[$(date)] install zsh-completions"
+if [ "$(uname)" == "Darwin" ];then
+  brew install zsh-completions
+fi
+
+# コマンドラインにgit の情報を表示させる
+echo "[$(date)] install git-prompt"
 if [ ! -e  ~/.zsh/prompt/git-prompt.sh ]; then
   mkdir -p ~/.zsh/prompt/
   cd ~/.zsh/prompt/ && wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/plugins/gitfast/git-prompt.sh 
   cd $BASEDIR
 fi
 
+# ホームディレクトリにシンボリックリンクを貼る
+echo "[$(date)] symlink dotfiles"
 function linkFile(){
   local SRCFILE=$1
   local DSTFILE=$2
@@ -18,6 +28,8 @@ function linkFile(){
   ln -s ${SRCFILE} ${DSTFILE}
 }
 
+# すでにシンボリックリンクが貼られていれば飛ばす
+# 既存のものはバックアップを取っておく
 function setFile() {
   local SRCFILE=$1
   local DSTFILE=$2
@@ -41,4 +53,13 @@ for f in ".zshrc" ".tmux.conf" ".gitconfig";
 do
   setFile "$BASEDIR/$f" "$HOME/$f"
 done
+
+# NeoBundleをインストール
+echo "$(date) install neobundle"
+if [ ! -e "~/.vim/bin/" ];then
+  mkdir -p ~/.vim/bin/
+fi
+curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh > ~/.vim/bin/install.sh
+sh ~/.vim/bin/install.sh
+
 
