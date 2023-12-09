f797591072c5b7e9b98fe9e1b646d708d286dd51
fullsat <fullsat310@gmail.com>
2023-12-09 12:04:37 +0900

    migrate from vundle to plug

--- bin/inst_vim.sh
+++ bin/inst_vim.sh
@@ -5,6 +5,5 @@ echo "$(date) install neobundle"
 if [ ! -e "~/.vim/bin/" ];then
   mkdir -p ~/.vim/bin/
 fi
-curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh > ~/.vim/bin/install.sh
-sh ~/.vim/bin/install.sh
-
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
